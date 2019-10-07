2d8b44cced8d6905e2566aede6e7c7e1bb812fc6
Brandon Pittman <brandon@brandonpittman.net>
2019-10-07 21:40:21 +0900

    wip

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    browser: true,
+    es6: true
+  },
+  plugins: ["vue", "gridsome"],
+  extends: [
+    "xo",
+    "plugin:vue/recommended",
+    "plugin:gridsome/recommended"
+  ]
+};
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -5,11 +5,21 @@
         <g-link to="/">{{ $static.metadata.siteName }}</g-link>
       </strong>
       <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
+        <g-link
+          class="nav__link"
+          to="/"
+        >
+          Home
+        </g-link>
+        <g-link
+          class="nav__link"
+          to="/about/"
+        >
+          About
+        </g-link>
       </nav>
     </header>
-    <slot/>
+    <slot />
   </div>
 </template>
 
