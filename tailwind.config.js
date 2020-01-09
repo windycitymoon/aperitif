6012473fb8897a7dd01167746344ce0d7066b181
Brandon Pittman <brandon@brandonpittman.net>
2020-01-09 18:40:56 +0900

    Implement Tailwind 1.2@canary.5

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 module.exports = {
-  // When Tailwind 1.2 is ready, the screen settings will be available as part of tailwindcss-plugin-fancy.
   theme: {
-    extend: {
-      screens: {
-        dark: { raw: '(prefers-color-scheme: dark)' },
-        light: { raw: '(prefers-color-scheme: light)' }
-      }
-    }
+    extend: {}
   },
   variants: {},
   plugins: [
